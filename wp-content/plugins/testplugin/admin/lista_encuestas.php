005e1b3caad20ddcd2bfface842fd477c1c22eee
albertohilal <albertohilal@github.com>
2024-10-19 11:15:23 -0300

    Actualización adicional en lista_encuestas.php. video 06-06-Obteniendo registros de la base de datos

--- wp-content/plugins/testplugin/admin/lista_encuestas.php
+++ wp-content/plugins/testplugin/admin/lista_encuestas.php
@@ -1,3 +1,12 @@
+ <?php
+    global $wpdb;
+    $query = " SELECT * FROM {$wpdb->prefix}encuestas";
+    $lista_encuestas = $wpdb->get_results($query, ARRAY_A);
+    if(empty($lista_encuestas )){
+        $lista_encuestas = array();
+    }
+    ?>
+    <div class="wrap">
  <div class="wrap">
  <?php
     echo "<h1>".get_admin_page_title()."</h1>";
@@ -11,15 +20,23 @@
                 <th >ShortCode</th>
                 <th >Acciones</th>
         </thead>
-        <tbody>
-            <tr>
-                <td>Primer encuesta</td>
-                <td>[CODE]</td>
-                <td>[encuesta id="1"]</td>
-                <td>
-                    <a class="page-title-action">Ver estadisticas</a>
-                    <a class="page-title-action">Borrar</a>
-                </td>
-            </tr>
+        <tbody id_"the-list">
+            <?php
+                foreach($lista_encuestas as $key => $value){
+                    $nombre = $value['Nombre'];
+                    $shortcode = $value['ShortCode'];
+                    echo "             
+                        <tr>
+                            <td>$nombre</td>
+                            <td>$shortcode</td>
+                            <td>
+                                <a class='page-title-action'>Ver estadisticas</a>
+                                <a class='page-title-action'>Borrar</a>
+                            </td>
+                        </tr>
+                    ";
+                }	
+                ?>
+            
         </tbody>
         </table>
